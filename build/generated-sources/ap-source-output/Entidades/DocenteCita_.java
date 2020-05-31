cf9bc0e1d51bfbfea77dbdac49aeb1dda58e147d
Kevin <kesgroom@gmail.com>
2020-05-31 10:17:29 -0500

    realizo pruebas del cambio de estilos

--- build/generated-sources/ap-source-output/Entidades/DocenteCita_.java
+++ build/generated-sources/ap-source-output/Entidades/DocenteCita_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-31T09:42:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-31T10:14:30")
 @StaticMetamodel(DocenteCita.class)
 public class DocenteCita_ { 
 
